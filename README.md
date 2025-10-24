c148115f87b7a92271ff3e474661ff6cd89e4218
ben-garber <55823257+ben-garber@users.noreply.github.com>
2025-10-23 22:54:05 -0500

    Make it argument based

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Vid2Gif
-
 A tool to convert videos to GIFs
+
+## Environment Setup
+This tool includes a virtual environment to run. <br>
+To setup the virtual environment, run `./setup.sh` from the top level directory of the repository. <br>
+This tool can also be run outside of the virtual environment if the dependencies listed in `requirements.txt` are installed.
+
+## Usage
+### Arguments
+- `-v`, `--video`: Path to the video file to convert
+  - Required: True
+  - Default: `None`
+- `-g`, `--gif`: Path to save the gif (including the name)
+  - Required: False
+  - Default: `converted.gif`
+- `-l`, `--loop`: Number of times the gif should loop (0 = infinite)
+  - Required: False
+  - Default: `0` (infinite)
+- `-d`, `--duration`: Number of milliseconds to display each frame
+  - Required: False
+  - Default: `100`
+### Example
+`python vid2gif.py -v ~/Downloads/video.mp4 -g test.gif -d 300 -l 0`
--- vid2gif.py
+++ vid2gif.py
@@ -5,8 +5,6 @@ import shutil
 import cv2
 from PIL import Image
 
-TEST_VID = '/Users/bengarber/Downloads/SDH.mp4'
-
 
 def get_frames(video_path: str, frame_dir: str) -> int:
     """Get frames of a video to a specified directory
@@ -23,7 +21,7 @@ def get_frames(video_path: str, frame_dir: str) -> int:
 
     frameNr = 0
 
-    while (True):
+    while True:
 
         success, frame = capture.read()
 
@@ -41,8 +39,61 @@ def get_frames(video_path: str, frame_dir: str) -> int:
 
 
 if __name__ == "__main__":
-    print(os.getcwd())
-    print(__file__)
+
+    parser = argparse.ArgumentParser(prog='Vid2Gif')
+    parser.add_argument('-v', '--video', help='Path to the video file', required=True)
+    parser.add_argument('-g', '--gif', help='Path to save the gif', default='converted.gif')
+    parser.add_argument('-l', '--loop', default=0,
+                        help='Number of times the GIF should loop (0 = infinite)')
+    parser.add_argument('-d', '--duration', default=100,
+                        help='Number of milliseconds to display each frame')
+
+    args = parser.parse_args()
+
+    # Verify video file exists
+    if not os.path.isfile(args.video):
+        print(f'Video file at path \'{args.video}\' does not exist.')
+        sys.exit(1)
+
+    # Verify gif file name
+    gif_good = False
+    if '.' in args.gif:
+        tmp = args.gif.split('.')
+        if tmp[-1] == 'gif':
+            gif_good = True
+    if not gif_good:
+        print(f'GIF name \'{args.gif}\' is invalid.')
+        sys.exit(1)
+
+    # Validate loop
+    loop_good = True
+    if type(args.loop) == int:
+        if args.loop < 0:
+            loop_good = False
+    elif type(args.loop) == str:
+        if not args.loop.isdigit():
+            loop_good = False
+    else:
+        print(f'Type error for loop arg ({type(args.loop)})')
+        sys.exit(1)
+    if not loop_good:
+        print('Loop must be a natural number.')
+        sys.exit(1)
+
+    # Validate duration
+    dur_good = True
+    if type(args.duration) == int:
+        if args.duration < 1:
+            dur_good = False
+    elif type(args.duration) == str:
+        if not args.duration.isdigit():
+            dur_good = False
+    else:
+        print(f'Type error for duration arg ({type(args.duration)})')
+        sys.exit(1)
+    if not dur_good:
+        print('Duration must be a positive integer.')
+        sys.exit(1)
 
     # Update this to be based off the file location rather than cwd
     frame_dir = os.path.join(os.getcwd(), 'tmp')
@@ -54,8 +105,7 @@ if __name__ == "__main__":
         os.mkdir(frame_dir)
 
     # Extract frames from video
-    get_frames(TEST_VID, frame_dir)
-    # print(os.listdir(frame_dir))
+    get_frames(args.video, frame_dir)
 
     # Make frame path template
     template = frame_dir + '/{}'
@@ -65,12 +115,12 @@ if __name__ == "__main__":
 
     # Save GIF
     frames[0].save(
-        "test.gif",
+        args.gif,
         format="GIF",
         append_images=frames[1:], # Append the rest of the frames
         save_all = True,
-        duration=300, # 100 milliseconds per frame
-        loop=0 # Infinite loop
+        duration=int(args.duration), # Milliseconds per frame
+        loop=int(args.loop) # Infinite loop
     )
 
     # Cleanup temp frames directory
